ff3e20e1866c5380386fe01f21a955f6e01ba243
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-07-03 21:02:25 -0300

    adiciona novo projeto para praticarr encapsulamento

---
+++ Java-POO/bytebank-encapsulado/src/bytebank_encapsulado/Conta.java
@@ -0,0 +1,62 @@
+package bytebank_encapsulado;
+
+
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+
+	public void deposita(double valor) {
+		this.saldo = this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			System.out.println("Você não tem saldo suficiente.");
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	public void setNumero(int novoNumero) {
+		this.numero = novoNumero;
+	}
+	
+	public void setAgencia(int novaAgencia) {
+		this.agencia = novaAgencia;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+}
---
+++ Java-POO/bytebank-encapsulado/src/bytebank_encapsulado/TestaGetSet.java
@@ -0,0 +1,22 @@
+package bytebank_encapsulado;
+
+public class TestaGetSet {
+	public static void main(String[] args) {
+		Conta contaTeste = new Conta();
+		contaTeste.setNumero(1337);
+		System.out.println(contaTeste.getNumero());
+		
+		Cliente sabrina = new Cliente();
+		
+		sabrina.setNome("Sabrina Gomes");
+		contaTeste.setTitular(sabrina);
+		
+		// mostra apenas o id
+		System.out.println(contaTeste.getTitular());
+
+		System.out.println(contaTeste.getTitular().getNome());
+		
+		contaTeste.getTitular().setProfissao("Programadora");
+
+	}
+}

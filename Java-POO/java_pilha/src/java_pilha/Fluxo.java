0eaae400a7579dc4cbf47eacefc89d8e730e159c
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-09 22:17:25 -0300

    adiciona bloco try/catch

--- Java-POO/java_pilha/src/java_pilha/Fluxo.java
+++ Java-POO/java_pilha/src/java_pilha/Fluxo.java
@@ -4,7 +4,11 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+        	metodo1();
+        } catch(ArithmeticException ex) {
+        	System.out.println("Divisão não pode ser realizada");
+        }
         System.out.println("Fim do main");
     }
 
@@ -18,7 +22,7 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-            
+            int a = i / 0;
         }
         System.out.println("Fim do metodo2");
     }
